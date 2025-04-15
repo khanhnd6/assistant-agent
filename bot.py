be991ba2c9ad4bf05ca2cd16f89874421448e071
khannd6 <khanhnd6@gmail.com>
2025-04-15 20:12:20 +0700

    change to schedule for items in 1 mins only

--- bot.py
+++ bot.py
@@ -278,7 +278,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def auto_message(context: CallbackContext):
     try:
-        collection = send_notifications(5)
+        collection = send_notifications(1)
         print("collection: ", collection)
         if collection:
             tasks = [context.bot.send_message(chat_id=user_id, text=msg)
